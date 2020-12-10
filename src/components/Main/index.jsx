a89c4bb849302eeafd74c96292972be1f9e4a6dc
Lucas Felix <lucasfelixdev@gmail.com>
2020-12-10 19:37:01 -0300

    feat: completion of the news and top games sidebar

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
+import { Container } from './styles'
+
 import ContentPrimary from '../pages/Home/ContentPrimary'
+import ContentSecondary from '../pages/Home/ContentSecondary'
 
 function Main() {
   return (
-    <>
+    <Container>
       <ContentPrimary />
-    </>
+      <ContentSecondary />
+    </Container>
   )
 }
 
